e79d782fe455170ea963b3050ffd975b7fa1163a
chaidarsaad <chaidarsaad55@gmail.com>
2025-03-13 16:27:04 +0700

    update: PWA

--- app/Http/Middleware/RunScheduleInBackground.php
+++ app/Http/Middleware/RunScheduleInBackground.php
@@ -16,11 +16,11 @@ class RunScheduleInBackground
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (!Cache::has('last_schedule_run')) {
-            exec('php ' . base_path('artisan') . ' schedule:run > /dev/null 2>/dev/null &');
+        // if (!Cache::has('last_schedule_run')) {
+        //     exec('php ' . base_path('artisan') . ' schedule:run > /dev/null 2>/dev/null &');
 
-            Cache::put('last_schedule_run', now(), 60);
-        }
+        //     Cache::put('last_schedule_run', now(), 60);
+        // }
 
         return $next($request);
     }
