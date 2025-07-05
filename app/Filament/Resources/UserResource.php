c7785d7685f74e4c3a38af669ae268ffa55b1466
chaidarsaad <chaidarsaad55@gmail.com>
2025-07-06 05:34:54 +0700

    Improves table infinite loading
    Removes the limit on infinite loading in the table.
    
    This allows all records to be loaded as the user scrolls,
    improving the user experience when browsing large datasets.

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -76,7 +76,7 @@ class UserResource extends Resource
     public static function table(Table $table): Table
     {
         return $table
-            ->infinite(5)
+            ->infinite()
             ->paginationPageOptions([5, 25, 50, 100, 250])
             ->defaultPaginationPageOption(5)
             ->defaultSort('id', direction: 'desc')
